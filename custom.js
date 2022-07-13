474bb3acf275693534cc207ba63f3d8e111fa14f
EsauKip <esau.kiprono@student.moringaschool.com>
2022-07-13 12:03:08 +0300

    initialize variables

---
+++ custom.js
@@ -0,0 +1,15 @@
+// hangman
+var alpha=new Array();
+var alpha_index=new Array();
+
+
+var bravo=new Array();
+var bravo_index=new Array();
+
+var runing=0;
+var failnum=0;
+var advising=0;
+
+function pick_word()
+{
+}
